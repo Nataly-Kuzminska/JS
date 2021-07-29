bb53c41b407d65d6f4043e15e3ec86a19fbbcb1f
Nataly Kuzminska <kuzminskanataly@gmail.com>
2021-07-29 19:58:23 +0300

    create variables

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-alert(5);
-console.log("Hi");
+
+let money = 0;
+let income = 0;
+let addExpences = 0;
+let deposit = 0;
+let mission = 0;
+let period = 0;
+alert('Hello, world!');
+console.log('Hi, friends!');
