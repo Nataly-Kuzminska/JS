013b615aabc4f8769b9f9f59158a0d370fa55409
Nataly Kuzminska <kuzminskanataly@gmail.com>
2021-07-30 19:43:19 +0300

    add lesson02

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 
-let money = 0;
-let income = 0;
-let addExpences = 0;
-let deposit = 0;
-let mission = 0;
-let period = 0;
-alert('Hello, world!');
-console.log('Hi, friends!');
+let money = 70000;
+let income = 'freelance';
+let addExpences = 'internet, taxi, medicine';
+let deposit = true;
+let mission = 400000;
+let period = 12;
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+console.log(addExpences.length);
+console.log('Период равен ' + period + ' месяцев');
+console('Цель заработать ' + mission + ' гривен');
+let newAddExpences = addExpences.toLowerCase().split('');
+console.log(newAddExpences);
+let budgetDay = money/30;
+console.log(budgetDay);
