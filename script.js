4c8d80a5a56d8af5184459447809c6a01c1244d7
Nataly Kuzminska <kuzminskanataly@gmail.com>
2021-08-02 15:49:59 +0300

    add lesson03

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-
-let money = 0;
-let income = 0;
-let addExpences = 0;
-let deposit = 0;
-let mission = 0;
-let period = 0;
-alert('Hello, world!');
-console.log('Hi, friends!');
+'use strict';
+let money = prompt('Какой ваш месячный доход?');
+let income = 'freelance';
+let addExpences = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+let deposit = !!(prompt('Есть ли у вас депозит в банке'));
+let expences1 = prompt('Введите обязательную статью расходов');
+let expences2 = prompt('Введите обязательную статью расходов');
+let amount1 = +(prompt('Во сколько это обойдется?'));
+let amount2 = +(prompt('Во сколько это обойдется?'));
+let budgetMonth = amount1 + amount2;
+console.log(budgetMonth);
+let mission = 400000;
+let period = Math.ceil(mission/budgetMonth);
+let budgetDay = budgetMonth/30;
+console.log(Math.floor(budgetDay));
+if (budgetDay >= 1200) {
+  console.log('Высокий уровень дохода')
+} else if (budgetDay >= 600 && budgetDay < 1200) {
+  console.log('У вас средний уровень дохода');
+} else if (budgetDay < 600 && budgetDay >= 0) {
+  console.log('К сожалению уровень дохода у вас ниже среднего');
+} else if (budgetDay < 0) {
+  console.log('Что-то пошло не так');
+}
