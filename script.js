26341ea81c065cfa2d2306ae34d14a4eb95d690f
Nataly Kuzminska <kuzminskanataly@gmail.com>
2021-08-03 18:43:10 +0300

    add lesson04

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 'use strict';
 let money = +prompt('Какой ваш месячный доход?');
 let addExpences = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-let deposit = !!(prompt('Есть ли у вас депозит в банке'));
+let deposit = confirm('Есть ли у вас депозит в банке?'));
 let expences1 = prompt('Введите обязательную статью расходов');
 let amount1 = +(prompt('Во сколько это обойдется?'));
 let expences2 = prompt('Введите обязательную статью расходов');
 let amount2 = +(prompt('Во сколько это обойдется?'));
-let budgetMonth = amount1 + amount2;
 let mission = 400000;
 let period = Math.ceil(mission/budgetMonth);
-let budgetDay = budgetMonth/30;
+let budgetDay = accumulatedMonth/30;
 
+function getExpencesMonth(amount1, amount2) {
+  return amount1 + amount2;
+}
+
+function getAccumulatedMonth(money, budgetMonth) {
+  let accumulatedMonth = getAccumulatedMonth();
+  return money - budgetMonth;
+  
+}
+
+
+function getTargetMonth(mission, accumulatedMonth) {
+  return mission/accumulatedMonth;
+}
+
+function showTypeOf() {
+  return typeof(income);
+}
+
+function getStatusIncome() {
 if (budgetDay >= 1200) {
   console.log('Высокий уровень дохода');
 } else if (budgetDay >= 600 && budgetDay < 1200) {
@@ -20,8 +39,13 @@ if (budgetDay >= 1200) {
 } else if (budgetDay < 0) {
   console.log('Что-то пошло не так');
 }
+}
 
-console.log(budgetMonth);
+showTypeOf();
+getExpencesMonth();
+getTargetMonth();
+getStatusIncome();
+console.log(...addExpences);
 console.log(Math.floor(budgetDay));
 
 
